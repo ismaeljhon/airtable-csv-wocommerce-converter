08949afde1f8bb6fb2b635778bd22e3a6c721f49
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2021-09-22 23:00:47 +0800

    Init files

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,119 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-csv-import v-model="jsonData" :map-fields="mapFields">
+      <vue-csv-toggle-headers></vue-csv-toggle-headers>
+      <vue-csv-errors></vue-csv-errors>
+      <vue-csv-input></vue-csv-input>
+      <vue-csv-map :auto-match="false"></vue-csv-map>
+    </vue-csv-import>
+    <div v-if="forWordpressJsonData.length">
+      <hr />
+      <download-csv :data="forWordpressJsonData">
+        <button>Download Data</button>
+      </download-csv>
+      <hr />
+      <table border="1">
+        <thead>
+          <tr>
+            <th v-for="(column, index) in mapFields" :key="index">
+              {{ column }}
+            </th>
+            <th>Tags</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in forWordpressJsonData" :key="index">
+            <td>{{ item.sku }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.description }}</td>
+            <td>
+              <ul>
+                <li v-for="imageName in item.images" :key="imageName">
+                  {{ imageName }}
+                </li>
+              </ul>
+            </td>
+            <td>{{ item.colors }}</td>
+            <td>{{ item.categories }}</td>
+            <td>{{ item.regular_price }}</td>
+            <td>{{ item.wholesale_price }}</td>
+            <td>{{ item.tags }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { VueCsvImport } from "vue-csv-import";
+import JsonCSV from "vue-json-csv";
+import _startCase from "lodash.startcase";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    VueCsvImport,
+    downloadCsv: JsonCSV,
+  },
+  watch: {
+    jsonData(data) {
+      const forWordpressJsonData = [];
+
+      data.forEach((item) => {
+        const {
+          name = "",
+          colors = "",
+          images = "",
+          regular_price = "",
+          wholesale_price = "",
+        } = item;
+
+        if (!name) return;
+
+        const colorArray = colors.split(",");
+
+        const updateItem = {
+          ...item,
+          images: images.split(","),
+          regular_price: regular_price.replace(/\$/g, ""),
+          wholesale_price: wholesale_price.replace(/\$/g, ""),
+        };
+
+        if (!colorArray) {
+          forWordpressJsonData.push(updateItem);
+          return;
+        }
+
+        colorArray.forEach((colorName) => {
+          const beautifyColorName = _startCase(colorName.toLowerCase());
+
+          forWordpressJsonData.push({
+            ...updateItem,
+            name: `${name.replace(/\./g, "")} - ${beautifyColorName}`,
+            tags: beautifyColorName,
+          });
+        });
+      });
+
+      this.forWordpressJsonData = forWordpressJsonData;
+    },
+  },
+  data: () => ({
+    jsonData: null,
+    forWordpressJsonData: [],
+    mapFields: [
+      "sku",
+      "name",
+      "description",
+      "images",
+      "colors",
+      "categories",
+      "regular_price",
+      "wholesale_price",
+    ],
+  }),
+};
 </script>
 
 <style>
@@ -21,7 +121,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
